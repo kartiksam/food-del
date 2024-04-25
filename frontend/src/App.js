20cbfc6615b776d9873af08d8cd64e357ebbdfc0
kartiksam <kartiksam123489@gmail.com>
2024-04-25 17:14:44 +0530

    header created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <div className="app">
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 };
