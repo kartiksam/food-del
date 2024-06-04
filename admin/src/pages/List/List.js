557e460e8e4ebc0f875ead9765f42447def5cd87
kartiksam <kartiksam123489@gmail.com>
2024-06-04 12:15:59 +0530

    list page not fully build

--- admin/src/pages/List/List.js
+++ admin/src/pages/List/List.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./List.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 const List = () => {
-  return <div></div>;
+  // create a state variable to store all the data from the db
+  const url = "http://localhost:4000";
+  const [list, setList] = useState([]);
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    console.log(response.data);
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+  // wqe need to run this function whenever the webpage is loaded
+  useEffect(() => {
+    fetchList();
+  }, []);
+  // now we displayt this data on this compoment
+  return (
+    <div className="list add flex-col">
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/` + item.image} alt="image" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p>x</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default List;
