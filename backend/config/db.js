98f5d9e280a5e0afaf391a03269f08a0f6af0cca
kartik sharma <kartiksam123489@gmail.com>
2025-03-31 22:54:06 +0530

    final changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 export const connnectDB = async () => {
   await mongoose
-    .connect(
-      // food-del is databse name inside collections will build
-      "mongodb+srv://kartiksam123489:22sharma88@food.qqxgjqe.mongodb.net/food-del"
-    )
+    .connect(process.env.MONGODB_URI)
+
     .then(() => {
       console.log("db connected");
     });
